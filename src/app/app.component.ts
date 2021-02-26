b4abd25e9ea70fcb7eb78e8587be60a3d92663c2
eastcoastdeveloper <uxdeveloper@protonmail.com>
2021-02-26 13:37:41 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import * as data from './cars.json';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent  {
+
+  cars:any = [];
+  data:any = data;
+
+  constructor(){
+    for(let key in data.muscleCars){
+      if(data.muscleCars.hasOwnProperty(key)){
+        this.cars.push(data.muscleCars[key])
+      }
+    }
+  }
+  
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, NgxPaginationModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { 
+
+
+
+}
