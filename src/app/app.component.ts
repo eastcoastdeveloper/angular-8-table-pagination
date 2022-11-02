c3c995be7f7c182c411a4b5b9c2a87220cd633e2
eastcoastdeveloper <58962475+eastcoastdeveloper@users.noreply.github.com>
2022-11-02 13:22:17 -0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,18 +12,15 @@ export class AppComponent implements OnInit{
   cars: CarsResponse[] = [];
   // data: any = data;
   p: any;
-  // totalVehicles = new Promise((resolve, reject) => {
-  //   setTimeout(() => {
-  //     resolve(this.cars.length);
-  //   }, 1000);
-  // });
+  totalVehicles: any = [];
 
   constructor(private _http: HttpClient) {
     // for (let key in data.muscleCars) {
     //   this.cars.push(data.muscleCars[key]);
     // }
     this._http.get('assets/cars.json').subscribe(val => {
-      console.log(val)
+      this.totalVehicles = val;
+      console.log(this.totalVehicles)
     });
   }
 
